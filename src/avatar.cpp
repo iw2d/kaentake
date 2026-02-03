94adb1b147b242ece745c942fd0086d06c4dfb36
iw2d <30405132+iw2d@users.noreply.github.com>
2026-02-03 11:43:12 +1000

    Implement tamingMob chairs

--- src/avatar.cpp
+++ src/avatar.cpp
@@ -3,6 +3,11 @@
 #include "wvs/avatar.h"
 #include "wvs/wvsapp.h"
 #include "wvs/iteminfo.h"
+#include "wvs/util.h"
+#include <windows.h>
+
+// Change tamingMob ID from 1983XXX -> 1939XXX
+#define TAMINGMOB_ID_PREFIX 1939
 
 
 void CAvatar::Constructor_hook() {
@@ -42,9 +47,31 @@ void __declspec(naked) CAvatar__Update_hook2() {
 }
 
 
+void CAvatar::SetRidingVehicle_hook(int nVehicleID) {
+    if (!m_bForcingAppearance) {
+        // CAvatar::ClearActionLayer(this, 0);
+        reinterpret_cast<void(__thiscall*)(CAvatar*, int)>(0x00453A29)(this, 0);
+        // Handle tamingMob chair
+        m_nRidingVehicleID = nVehicleID;
+        if (!nVehicleID && m_pCustomData->nRidingChairID) {
+            m_nRidingVehicleID = m_pCustomData->nRidingChairID;
+        }
+        PrepareActionLayer(6, 100, 0);
+    }
+}
+
+static auto load_tamingmob_action = reinterpret_cast<void(__cdecl*)(int, int, int, void*)>(0x00413A43);
+void __cdecl load_tamingmob_action_hook(int nAction, int nMobBodyID, int nID, void* aFrame) {
+    if (nMobBodyID / 1000 == TAMINGMOB_ID_PREFIX) {
+        nAction = 39; // ACT_SIT
+    }
+    load_tamingmob_action(nAction, nMobBodyID, nID, aFrame);
+}
+
+
 class CUser {
 public:
-    MEMBER_AT(CAvatar, 0x88, m_Avatar)
+    MEMBER_AT(CAvatar, 0x88, m_CAvatar)
 
     MEMBER_HOOK(void, 0x00930B27, Update)
     MEMBER_HOOK(void, 0x0093C7C3, SetActivePortableChair, int nItemID)
@@ -52,30 +79,40 @@ public:
 
 void CUser::Update_hook() {
     CUser::Update(this);
-    POINT p = m_Avatar.m_pCustomData->ptBodyRelMove;
+    POINT p = m_CAvatar.m_pCustomData->ptBodyRelMove;
     if (p.x || p.y) {
-        m_Avatar.m_pBodyOrigin->RelMove(p.x, p.y);
+        m_CAvatar.m_pBodyOrigin->RelMove(p.x, p.y);
     }
 }
 
 void CUser::SetActivePortableChair_hook(int nItemID) {
     CUser::SetActivePortableChair(this, nItemID);
     if (nItemID == 0) {
-        m_Avatar.m_pCustomData->ptBodyRelMove = {0, 0};
+        m_CAvatar.m_pCustomData->ptBodyRelMove = {0, 0};
+        if (m_CAvatar.m_nRidingVehicleID / 1000 == TAMINGMOB_ID_PREFIX) {
+            m_CAvatar.m_pCustomData->nRidingChairID = 0;
+            m_CAvatar.SetRidingVehicle_hook(0);
+        }
         return;
     }
-    // Resolve bodyRelMove offsets
     IWzPropertyPtr pInfo = CItemInfo::GetInstance()->GetItemInfo(nItemID);
     if (!pInfo) {
         return;
     }
+    // Resolve tamingMob property
+    int nTamingMob = get_int32(pInfo->item[L"tamingMob"], 0);
+    if (nTamingMob && nTamingMob / 1000 == TAMINGMOB_ID_PREFIX) {
+        m_CAvatar.m_pCustomData->nRidingChairID = nTamingMob;
+        m_CAvatar.SetRidingVehicle_hook(nTamingMob);
+    }
+    // Resolve bodyRelMove offsets
     IWzVector2DPtr pVector;
     IUnknownPtr pUnknown = pInfo->item[L"bodyRelMove"].GetUnknown();
     if (FAILED(pUnknown.QueryInterface(__uuidof(IWzVector2D), &pVector))) {
         return;
     }
     // Store to CAvatar->m_pCustomData struct
-    m_Avatar.m_pCustomData->ptBodyRelMove = {pVector->x, pVector->y};
+    m_CAvatar.m_pCustomData->ptBodyRelMove = {pVector->x, pVector->y};
 }
 
 
@@ -86,6 +123,10 @@ void AttachAvatarDataMod() {
     ATTACH_HOOK(CAvatar::RegisterNextBlink, CAvatar::RegisterNextBlink_hook);
     PatchJmp(CAvatar__Update_jmp1, CAvatar__Update_hook1);
     PatchJmp(CAvatar__Update_jmp2, CAvatar__Update_hook2);
+
+    // Implement tamingMob
+    ATTACH_HOOK(CAvatar::SetRidingVehicle, CAvatar::SetRidingVehicle_hook);
+    PatchCall(0x00413A31, load_tamingmob_action_hook); // CActionMan::LoadTamingMobAction
 
     // Implement bodyRelMove
     ATTACH_HOOK(CUser::Update, CUser::Update_hook);
