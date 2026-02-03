5479ee58069e3b227b565ce724e4e6410ba65589
iw2d <30405132+iw2d@users.noreply.github.com>
2026-02-03 11:43:12 +1000

    Implement tamingMob chairs

--- src/wvs/avatar.h
+++ src/wvs/avatar.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "hook.h"
 #include "ztl/ztl.h"
+#include <windows.h>
 
 
 class CAvatar {
@@ -8,6 +9,7 @@ public:
     struct CustomData {
         int bBlinking;
         POINT ptBodyRelMove;
+        int nRidingChairID;
     };
 
     virtual ~CAvatar() = 0;
@@ -17,12 +19,15 @@ public:
     virtual void SetMoveAction(int nMA, int bReload) = 0;
     virtual void PrepareActionLayer(int nActionSpeed, int nWalkSpeed, int bKeyDown) = 0;
 
+    MEMBER_AT(int, 0x460, m_bForcingAppearance)
     MEMBER_AT(CustomData*, 0x484, m_pCustomData) // Hijack m_bBlinking
     MEMBER_AT(int, 0x488, m_tNextBlink)
+    MEMBER_AT(int, 0x4BC, m_nRidingVehicleID)
     MEMBER_AT(IWzVector2DPtr, 0x10B8, m_pBodyOrigin)
 
     // avatar.cpp
     MEMBER_HOOK(void, 0x0044FE6C, Constructor)
     MEMBER_HOOK(void, 0x0045011C, Destructor)
     MEMBER_HOOK(void, 0x00453AA2, RegisterNextBlink)
+    MEMBER_HOOK(void, 0x00456E35, SetRidingVehicle, int nVehicleID)
 };
