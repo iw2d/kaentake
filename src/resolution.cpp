f98c4c019fdfc6cdab3abd9a4128cb3ebef83ded
iw2d <30405132+iw2d@users.noreply.github.com>
2026-04-02 23:02:06 +1000

    Port common changes from custom-ui branch
    - Noted an ABI compatibility issue with multiple inheritance and virtual destructors - GMS clients v91 and older are compiled using VS2006, and align virtual destructors to `+0x8` (ZRefCounted vftable) whereas VS2008+ compilers align them to `0x0`.

--- src/resolution.cpp
+++ src/resolution.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "hook.h"
 #include "constants.h"
+#include "wvs/config.h"
 #include "wvs/wnd.h"
 #include "wvs/wndman.h"
 #include "wvs/tooltip.h"
@@ -123,30 +124,6 @@ void __cdecl set_stage_hook(CStage* pStage, void* pParam) {
     }
 }
 
-
-class CConfig : public TSingleton<CConfig, 0x00BEBF9C> {
-public:
-    enum {
-        GLOBAL_OPT = 0x0,
-        LAST_CONNECT_INFO = 0x1,
-        CHARACTER_OPT = 0x2,
-    };
-    MEMBER_ARRAY_AT(int, 0xCC, m_nUIWnd_X, 34)
-    MEMBER_ARRAY_AT(int, 0x154, m_nUIWnd_Y, 34)
-
-    MEMBER_HOOK(void, 0x0049F0B5, GetUIWndPos, int nUIType, int* x, int* y, int* op)
-    MEMBER_HOOK(void, 0x0049D0B6, LoadCharacter, int nWorldID, unsigned int dwCharacterId)
-    MEMBER_HOOK(void, 0x0049C441, LoadGlobal)
-    MEMBER_HOOK(void, 0x0049C8E7, SaveGlobal)
-    MEMBER_HOOK(void, 0x0049EA33, ApplySysOpt, void* pSysOpt, int bApplyVideo)
-
-    int GetOpt_Int(int nType, const char* sKey, int nDefaultX, int nLowBound, int nHighBound) {
-        return reinterpret_cast<int(__thiscall*)(CConfig*, int, const char*, int, int, int)>(0x0049EF65)(this, nType, sKey, nDefaultX, nLowBound, nHighBound);
-    }
-    void SetOpt_Int(int nType, const char* sKey, int nValue) {
-        reinterpret_cast<void(__thiscall*)(CConfig*, int, const char*, int)>(0x0049EFB5)(this, nType, sKey, nValue);
-    }
-};
 
 void CConfig::GetUIWndPos_hook(int nUIType, int* x, int* y, int* op) {
     CConfig::GetUIWndPos(this, nUIType, x, y, op);
--- src/wvs/ctrlwnd.h
+++ src/wvs/ctrlwnd.h
@@ -2,13 +2,17 @@
 #include "hook.h"
 #include "wvs/gobj.h"
 #include "wvs/msghandler.h"
-#include "wvs/wnd.h"
 #include "ztl/ztl.h"
 #include <windows.h>
 
 
+class CWnd;
+struct DRAGCTX;
+
 class CCtrlWnd : public IGObj, public IUIMsgHandler, public ZRefCounted {
 public:
+    unsigned char pad0[0x34 - sizeof(IGObj) - sizeof(IUIMsgHandler) - sizeof(ZRefCounted)];
+
     virtual void Update() override {}
     virtual int OnDragDrop(int nState, DRAGCTX& ctx, int rx, int ry) { return 0; }
     virtual void CreateCtrl(CWnd* pParent, unsigned int nId, int l, int t, int w, int h, void* pData) {}
@@ -18,6 +22,35 @@ public:
     virtual int HitTest(unsigned int rx, unsigned int ry) { return 0; }
     virtual void Draw(int rx, int ry, const RECT* pRect) {}
 };
+static_assert(sizeof(CCtrlWnd) == 0x34);
+
+
+class CCtrlButton : public CCtrlWnd {
+public:
+    struct CREATEPARAM {
+        int bAcceptFocus;
+        int bDrawBack;
+        int bAnimateOnce;
+        ZXString<wchar_t> sUOL;
+
+        CREATEPARAM() : bAcceptFocus(0), bDrawBack(0), bAnimateOnce(0) {
+        }
+        ~CREATEPARAM() {
+        }
+    };
+    static_assert(sizeof(CREATEPARAM) == 0x10);
+
+    unsigned char pad0[0x5A4 - sizeof(CCtrlWnd)];
+
+    virtual void CreateCtrl(CWnd* pParent, unsigned int nId, int l, int t, int decClickArea, void* pData) {}
+    virtual ~CCtrlButton() {
+        reinterpret_cast<void(__thiscall*)(CCtrlButton*)>(0x00425A3B)(reinterpret_cast<CCtrlButton*>(reinterpret_cast<uintptr_t>(this) + 0x8));
+    }
+    CCtrlButton() {
+        reinterpret_cast<void(__thiscall*)(CCtrlButton*)>(0x004258E4)(this);
+    }
+};
+static_assert(sizeof(CCtrlButton) == 0x5A4);
 
 
 class CCtrlComboBox : public CCtrlWnd {
@@ -39,13 +72,13 @@ public:
         }
     };
 
+    virtual void CreateCtrl(void* pParent, unsigned int nId, int nType, int l, int t, int w, int h, void* pData) {}
+    virtual ~CCtrlComboBox() override {
+        reinterpret_cast<void(__thiscall*)(CCtrlComboBox*)>(0x004C4372)(reinterpret_cast<CCtrlComboBox*>(reinterpret_cast<uintptr_t>(this) + 0x8));
+    }
     CCtrlComboBox() {
         reinterpret_cast<void(__thiscall*)(CCtrlComboBox*)>(0x004C4259)(this);
     }
-    virtual ~CCtrlComboBox() override {
-        reinterpret_cast<void(__thiscall*)(CCtrlComboBox*)>(0x004C4372)(this);
-    }
-    virtual void CreateCtrl(void* pParent, unsigned int nId, int nType, int l, int t, int w, int h, void* pData) {}
 
     void AddItem(const char* sItemName, unsigned int dwParam) {
         reinterpret_cast<void(__thiscall*)(CCtrlComboBox*, const char*, unsigned int)>(0x004C6DD6)(this, sItemName, dwParam);
@@ -54,3 +87,4 @@ public:
         reinterpret_cast<void(__thiscall*)(CCtrlComboBox*, int)>(0x004C738B)(this, nSelect);
     }
 };
+static_assert(sizeof(CCtrlComboBox) == 0x10C);
